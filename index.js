de34b22808f971759c61c83c228a7fdfd042a69a
Md. Alif <110423275+alif68094@users.noreply.github.com>
2023-10-18 22:30:01 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ async function run() {
 
     const productCollection = client.db('productDB').collection('product')
 
-    app.post('/newProduct', async(req, res) => {
+
+    app.get('/product', async(req, res) => {
+        const cursor = productCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.post('/Product', async(req, res) => {
         const newProduct = req.body;
         console.log(newProduct);
         const result = await productCollection.insertOne(newProduct)
